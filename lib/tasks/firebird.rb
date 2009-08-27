c5568f50a655efdae5363ac5b19b3d41b5a74778
Artur Roszczyk <sevos@home.eax.pl>
2009-08-28 00:31:56 +0200

    added firebird ActiveRecord adapter build and install tasks

--- lib/tasks/firebird.rb
+++ lib/tasks/firebird.rb
@@ -16,10 +16,26 @@ namespace :firebird do
         run "rm -fr /tmp/fb"
       end
     end
+
+    namespace :fb_adapter do
+      desc "Build AR Firebird adapter"
+      task :build do
+        info "Downloading and building fb_adapter"
+        run "cd /tmp; git clone git://github.com/rowland/fb_adapter.git"
+        run "cd /tmp/fb_adapter; rake gem"
+      end
+      desc "Install AR Firebird adapter"
+      task :install => :build do
+        info "Installing fb_adapter"
+        install_cmd = GemPackage.install_path Dir['/tmp/fb_adapter/pkg/fb*.gem'].first
+        run "cd /tmp/fb_adapter; #{install_cmd}"
+        run "rm -fr /tmp/fb_adapter"
+      end
+    end
   end
 
   desc "Install firebird-superserver and RoR adapters"
-  task :all => ["install", "gem:fireruby:install"]
+  task :all => ["install", "gem:fireruby:install", "gem:fb_adapter:install"]
 end
    
    
