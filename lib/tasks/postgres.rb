82994f97db199d9ffae1ef4bcc5a66fca491aa67
Artur Roszczyk <sevos@home.eax.pl>
2009-08-28 15:10:04 +0200

    added pg_hba.conf finder

--- lib/tasks/postgres.rb
+++ lib/tasks/postgres.rb
@@ -14,7 +14,8 @@ namespace :postgres do
 
   desc "Enable local user access"
   task :enable_local_access do
-    puts "*** \e[1;31mNot implemented yet\e[0m ***"
+    puts "*** \e[1;31mEnabling local access is not implemented yet\e[0m ***"
+    puts "*** \e[1;31mYou need to edit #{find_pg_hba_conf}\e[0m ***"
   end
 
 
@@ -23,7 +24,8 @@ namespace :postgres do
   if distribution == :fedora
     desc "Fedora: initialize cluster"
     task :create_cluster do
-        puts "*** \e[1;31mNot implemented yet\e[0m ***"
+      info "Initializing database cluster"  
+      surun "#{Dir["/etc/init.d/postgres*"].first} initdb"
     end
     all_tasks.insert(2, :create_cluster)
   end
@@ -31,4 +33,13 @@ namespace :postgres do
     desc "Install and configure PostgreSQL server"
     task :all => all_tasks
 
+end
+
+def find_pg_hba_conf
+  cmd = "find /var /etc -name pg_hba.conf"
+  cmd = DISTRIBUTIONS[distribution][:admin_cmd].gsub("+cmd+", cmd)
+  info "Searching pg_hba.conf"
+  info DISTRIBUTIONS[distribution][:admin_notify]
+  result = `#{cmd}`
+  result.split("\n").first
 end
