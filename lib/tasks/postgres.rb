f030ae8fe8ce7d25c18e65061d7835cc9826c787
Artur Roszczyk <sevos@home.eax.pl>
2009-08-28 14:41:54 +0200

    refactoring postgres:passwd

--- lib/tasks/postgres.rb
+++ lib/tasks/postgres.rb
@@ -1,22 +1,15 @@
 namespace :postgres do
   
   desc "Pre-Configure PostgreSQL server"
-  task :postgres_passwd do
-    case distribution
-      when :fedora 
-        passwd_cmd = 'su -c "su -l postgres -c psql postgres"'
-        usr = "Enter your root password"
-      when :ubuntu 
-        passwd_cmd = 'sudo -u postgres psql postgres'
-        usr = "Enter your password if nessesary"
-    end
-    info "#{usr} and when you see"
+  task :passwd do
+    
+    info "When you see"
     info "postgres=# prompt type:"
     info "\\passwd postgres"
     info "and then set postgres user password"
     info "it should be strong password"
     info "(postgres user has administrative privileges)"
-    run passwd_cmd
+    surun "su -l postgres -c psql postgres"
   end
 
   desc "Enable local user access"
