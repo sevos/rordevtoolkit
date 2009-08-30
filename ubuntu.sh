3ca1d7dad00ada63cc613d588f4cde0ee12a68a6
Artur Roszczyk <sevos@home.eax.pl>
2009-08-30 02:07:33 +0200

    exporting rake path in ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,6 +6,7 @@ sleep 5
 sudo apt-get -y install ruby ruby-dev rubygems irb
 sudo gem install rake
 echo "export PATH=/var/lib/gems/1.8/bin:\$PATH" >> ~/.bashrc
+export PATH=/var/lib/gems/1.8/bin:$PATH
 echo "*** Common tasks ***"
-/var/lib/gems/1.8/bin/rake -T | grep -e "\:all"
+rake -T | grep -e "\:all"
 echo "Enter\n    rake -T\nfor details"
