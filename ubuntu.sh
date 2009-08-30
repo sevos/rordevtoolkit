f4ec7ff05ace8236d9136e649faca5ebfc0f0d20
Artur Roszczyk <sevos@home.eax.pl>
2009-08-30 02:01:48 +0200

    fixed rake call in ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo -e "This script will prepare your Ubuntu system to use RoR Development Tools"
-echo -e "Press ctrl-c to cancel. Process will start in 5 seconds"
+echo "This script will prepare your Ubuntu system to use RoR Development Tools"
+echo "Press ctrl-c to cancel. Process will start in 5 seconds"
 sleep 5
 sudo apt-get -y install ruby ruby-dev rubygems
 echo "export PATH=/var/lib/gems/1.8/bin:\$PATH" >> ~/.bashrc
 echo "*** Common tasks ***"
-rake -T | grep -e "\:all"
+/var/lib/gems/1.8/bin/rake -T | grep -e "\:all"
 echo "Enter\n    rake -T\nfor details"
