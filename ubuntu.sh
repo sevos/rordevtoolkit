47448036605cc99073899553f1797886c6ee3abc
Artur Roszczyk <sevos@home.eax.pl>
2009-08-30 02:02:42 +0200

    added irb to ubuntu.sh and changed README

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,7 +3,7 @@
 echo "This script will prepare your Ubuntu system to use RoR Development Tools"
 echo "Press ctrl-c to cancel. Process will start in 5 seconds"
 sleep 5
-sudo apt-get -y install ruby ruby-dev rubygems
+sudo apt-get -y install ruby ruby-dev rubygems irb
 echo "export PATH=/var/lib/gems/1.8/bin:\$PATH" >> ~/.bashrc
 echo "*** Common tasks ***"
 /var/lib/gems/1.8/bin/rake -T | grep -e "\:all"
