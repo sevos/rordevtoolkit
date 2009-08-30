d4452033bf0788ca36c232c953de79070bee3fc3
Artur Roszczyk <sevos@home.eax.pl>
2009-08-30 02:04:45 +0200

    added rake gem install in ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,6 +4,7 @@ echo "This script will prepare your Ubuntu system to use RoR Development Tools"
 echo "Press ctrl-c to cancel. Process will start in 5 seconds"
 sleep 5
 sudo apt-get -y install ruby ruby-dev rubygems irb
+sudo gem install rake
 echo "export PATH=/var/lib/gems/1.8/bin:\$PATH" >> ~/.bashrc
 echo "*** Common tasks ***"
 /var/lib/gems/1.8/bin/rake -T | grep -e "\:all"
