297740fa0a5c88189eb935f38133172e0724104a
Artur Roszczyk <sevos@home.eax.pl>
2009-08-30 12:05:25 +0200

    added libopenssl-ruby to install list in ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,7 +3,7 @@
 echo "This script will prepare your Ubuntu system to use RoR Development Tools"
 echo "Press ctrl-c to cancel. Process will start in 5 seconds"
 sleep 5
-sudo apt-get -y install ruby ruby-dev rubygems irb
+sudo apt-get -y install ruby ruby-dev rubygems irb libopenssl-ruby
 sudo gem install rake
 echo "export PATH=/var/lib/gems/1.8/bin:\$PATH" >> ~/.bashrc
 export PATH=/var/lib/gems/1.8/bin:$PATH
