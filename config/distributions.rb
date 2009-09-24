bdecaad6bdeab6faa5a0ce10dacc808db3676ed4
Artur Roszczyk <sevos@home.eax.pl>
2009-09-24 09:55:02 +0200

    clearing files

--- config/distributions.rb
+++ config/distributions.rb
@@ -3,19 +3,11 @@ DISTRIBUTIONS = {
     :check => {
       :file_exists => "/usr/bin/yum" 
     },
-    :install_path => 'yum -yq install +pkg_names+',
-    :gem_install_path => 'gem install +pkg_names+',
-    :admin_notify => "Enter your root password below",
-    :admin_cmd => 'su -c "+cmd+"'
   },
   :ubuntu => {
     :check => {
       :file_exists => "/usr/bin/apt-get" 
     },
-    :install_path => 'apt-get -y install +pkg_names+',
-    :gem_install_path => 'gem install +pkg_names+',
-    :admin_notify => "Sometimes you need enter your user password here",
-    :admin_cmd => 'sudo /bin/sh -c "+cmd+"'
   }
 }
 
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -3,49 +3,6 @@ require 'config/distributions.rb'
 PACKAGES = YAML.load_file('config/packages.yml')
 @@distribution = nil
 
-class SoftwarePackage
-  def self.install_path(file)
-    file ||= ""
-    DISTRIBUTIONS[distribution][:install_path].gsub('+pkg_names+', file)
-  end
-  def self.install_package_path(package)
-    pkglist = PACKAGES["app"][package.to_s][distribution.to_s]
-    install_path pkglist
-  end
-end
-
-class GemPackage
-  def self.install_path(file)
-    DISTRIBUTIONS[distribution][:gem_install_path].gsub('+pkg_names+', file)
-  end
-  def self.install_package_path(package)
-    pkglist = PACKAGES["gem"][package.to_s]
-
-    # remove installed gems from list
-    pkglist = pkglist.split(' ').reject {|pkg| installed? pkg}.join(' ')
-    
-    install_path pkglist unless pkglist.nil? or pkglist == ""
-  end
-  def self.installed?(gemname)
-    if `gem list | grep "#{gemname.to_s}"` == ""
-      false
-    else
-      true
-    end
-  end
-end
-
-def pinstall(package)
-  info "Installing #{package.to_s}"
-  surun SoftwarePackage.install_package_path(package)
-end
-
-def ginstall(package)
-  info "Installing gem #{package.to_s}"
-  path = GemPackage.install_package_path(package)
-  surun path
-end
-
 def distribution
   if not @@distribution
     DISTRIBUTIONS.each do |dist, options|
@@ -60,24 +17,9 @@ def distribution
   @@distribution
 end
 
-def run(cmd)
-  if cmd
-    puts "\e[32mExecuting: \e[34m#{cmd}\e[0m" 
-    system cmd  
-  end
-end
-
-def surun(cmd)
-  if cmd
-    info DISTRIBUTIONS[distribution][:admin_notify] if DISTRIBUTIONS[distribution][:admin_notify]
-    run DISTRIBUTIONS[distribution][:admin_cmd].gsub('+cmd+', cmd)
-  end
-end
-
 def home_path
   `echo -n $HOME`
 end
-
 
 
 
