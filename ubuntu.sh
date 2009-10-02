de2198d8128a384c7ff6aea55df0e700e67a926e
Aleksander Pohl <apohllo@o2.pl>
2009-10-02 14:09:33 +0200

    Adding 'make' to installation script (not installed by default on Debina)

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,7 +3,7 @@
 echo "This script will prepare your Ubuntu system to use RoR Development Tools"
 echo "Press ctrl-c to cancel. Process will start in 5 seconds"
 sleep 5
-sudo apt-get -y install ruby ruby-dev rubygems irb libopenssl-ruby
+sudo apt-get -y install make ruby ruby-dev rubygems irb libopenssl-ruby 
 sudo gem install rake
 echo "export PATH=/var/lib/gems/1.8/bin:\$PATH" >> ~/.bashrc
 export PATH=/var/lib/gems/1.8/bin:$PATH
