bc3b348a7f639052d1100e2d0934f89ecaa3f6d7
Aleksander Pohl <apohllo@o2.pl>
2009-10-15 00:51:10 +0200

    - Debian script name changed - Missing PATH variable added

--- debian.sh
+++ debian.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "This script will prepare your Ubuntu system to use RoR Development Tools"
+echo "This script will prepare your Debian system to use RoR Development Tools"
 echo "Press ctrl-c to cancel. Process will start in 5 seconds"
 sleep 5
 sudo apt-get -y install make ruby ruby-dev rubygems irb libopenssl-ruby libfcgi-dev g++
@@ -9,7 +9,7 @@ echo "export PATH=/var/lib/gems/1.8/bin:\$PATH" >> ~/.bashrc
 export PATH=/var/lib/gems/1.8/bin:$PATH
 echo "** Updating rubygems"
 sudo gem install rubygems-update -v 1.3.1
-sudo update_rubygems
+sudo PATH=/var/lib/gems/1.8/bin:$PATH update_rubygems
 sudo gem install hoe -v 2.0.0
 rake module:rubygems:install
 echo "** Tasks"
